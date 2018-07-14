af10f1b2e47b7c619e7f49214c0e499cf6072e6f
Jordan <jarenstein@icloud.com>
2018-07-14 22:09:53 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -2,205 +2,176 @@ import { remote } from "electron"
 const { TouchBar, nativeImage } = remote
 const { TouchBarButton, TouchBarLabel, TouchBarSpacer } = TouchBar
 import * as Oni from "oni-api"
-import { join } from "path"
+import * as path from "path"
 
-interface Icons {
-  browser: Electron.nativeImage
-  browserBack: Electron.nativeImage
-  browserForward: Electron.nativeImage
-  debug: Electron.nativeImage
-  definition: Electron.nativeImage
-  explorer: Electron.nativeImage
-  format: Electron.nativeImage
-  learn: Electron.nativeImage
-  marks: Electron.nativeImage
-  plugins: Electron.nativeImage
-  recordStart: Electron.nativeImage
-  recordStop: Electron.nativeImage
-  reload: Electron.nativeImage
-  rename: Electron.nativeImage
-  search: Electron.nativeImage
-  sidebar: Electron.nativeImage
+const namedImage = (name: String): Electron.nativeImage => {
+  const image: Electron.nativeImage = nativeImage.createFromNamedImage(
+    `NSTouchBar${name}Template`,
+    null
+  )
+  image.setTemplateImage(true)
+  return image
 }
-export const activate = (oni: Oni.Plugin.Api) => {
-  const shift = [0, 0, 0]
-  const icons: Icons = {
-    browser: nativeImage.createFromNamedImage("NSTouchBarOpenInBrowserTemplate", shift),
-    browserBack: nativeImage.createFromNamedImage("NSTouchBarGoBackTemplate", shift),
-    browserForward: nativeImage.createFromNamedImage("NSTouchBarGoForwardTemplate", shift),
-    debug: nativeImage.createFromPath(join(__dirname, "..", "icons/debug.png")),
-    definition: nativeImage.createFromPath(
-      join(__dirname, "..", "icons/definition.png")
-    ),
-    explorer: nativeImage.createFromPath(
-      join(__dirname, "..", "icons/explorer.png")
-    ),
-    format: nativeImage.createFromPath(
-      join(__dirname, "..", "icons/format.png")
-    ),
-    learn: nativeImage.createFromPath(join(__dirname, "..", "icons/learn.png")),
-    marks: nativeImage.createFromPath(join(__dirname, "..", "icons/marks.png")),
-    plugins: nativeImage.createFromPath(
-      join(__dirname, "..", "icons/plugins.png")
-    ),
-    recordStart: nativeImage.createFromNamedImage(
-      "NSTouchBarRecordStartTemplate",
-      shift
-    ),
-    recordStop: nativeImage.createFromNamedImage(
-      "NSTouchBarRecordStopTemplate",
-      shift
-    ),
-    reload: nativeImage.createFromNamedImage(
-      "NSTouchBarRefreshTemplate",
-      shift
-    ),
-    rename: nativeImage.createFromPath(
-      join(__dirname, "..", "icons/rename.png")
-    ),
-    search: nativeImage.createFromNamedImage("NSTouchBarSearchTemplate", shift),
-    sidebar: nativeImage.createFromNamedImage(
-      "NSTouchBarSidebarTemplate",
-      shift
-    ),
-  }
-  for (let name in icons) {
-    icons[name].setTemplateImage(true)
-  }
 
+const localImage = (name: String): Electron.nativeImage => {
+  const image: Electron.nativeImage = nativeImage.createFromPath(
+    path.join(__dirname, "..", `icons/${name}.png`)
+  )
+  image.setTemplateImage(true)
+  return image
+}
+
+const icons = {
+  browser: namedImage("OpenInBrowser"),
+  browserBack: namedImage("GoBack"),
+  browserForward: namedImage("GoForward"),
+  debug: localImage("Debug"),
+  definition: localImage("Definition"),
+  explorer: localImage("Explorer"),
+  format: localImage("Format"),
+  learn: localImage("Learn"),
+  marks: localImage("Marks"),
+  plugins: localImage("Plugins"),
+  recordStart: namedImage("RecordStart"),
+  recordStop: namedImage("RecordStop"),
+  reload: namedImage("Refresh"),
+  rename: localImage("Rename"),
+  search: namedImage("Search"),
+  sidebar: namedImage("Sidebar"),
+}
+
+export const activate = (oni: Oni.Plugin.Api) => {
   const biggerEscButton = new TouchBarButton({
     label: "escape",
-    click: () => {
-      oni.editors.activeEditor.neovim.input("<esc>")
-    },
+    click: () => oni.editors.activeEditor.neovim.input("<esc>"),
   })
 
+  // sidebar commands
   const sidebarButton = new TouchBarButton({
     icon: icons.sidebar,
-    click: () => {
-      oni.commands.executeCommand("sidebar.toggle")
-    },
+    click: () => oni.commands.executeCommand("sidebar.toggle"),
   })
 
   const explorerButton = new TouchBarButton({
     icon: icons.explorer,
-    click: () => {
-      oni.commands.executeCommand("explorer.toggle")
-    },
+    click: () => oni.commands.executeCommand("explorer.toggle"),
   })
 
   const searchButton = new TouchBarButton({
     icon: icons.search,
-    click: () => {
-      oni.commands.executeCommand("search.searchAllFiles")
-    },
+    click: () => oni.commands.executeCommand("search.searchAllFiles"),
   })
 
-  const renameButton = new TouchBarButton({
-    icon: icons.rename,
-    click: () => {
-      oni.commands.executeCommand("editor.rename")
-    },
-  })
-
-  const definitionButton = new TouchBarButton({
-    icon: icons.definition,
-    click: () => {
-      oni.commands.executeCommand("language.gotoDefinition")
-    },
-  })
-
-  const formatButton = new TouchBarButton({
-    icon: icons.format,
-    click: () => {
-      oni.commands.executeCommand("autoformat.prettier")
-    },
-  })
-
-  const recordButton = new TouchBarButton({
-    icon: icons.recordStart,
-    backgroundColor: "#FF0000",
-    click: () => {
-      if (oni.recorder.isRecording) {
-        oni.commands.executeCommand("keyDisplayer.hide")
-        oni.recorder.stopRecording()
-        recordButton.icon = icons.recordStart
-      } else {
-        oni.commands.executeCommand("keyDisplayer.show")
-        oni.recorder.startRecording()
-        recordButton.icon = icons.recordStop
-      }
-    },
-  })
-
-  const developerToolsButton = new TouchBarButton({
-    icon: icons.debug,
-    click: () => {
-      oni.commands.executeCommand("oni.debug.openDevTools")
-    },
-  })
-
-  const reloadButton = new TouchBarButton({
-    icon: icons.reload,
-    click: () => {
-      oni.commands.executeCommand("oni.debug.reload")
-    },
-  })
-
+  // browser commands
   const browserButton = new TouchBarButton({
     icon: icons.browser,
-    click: () => {
-      oni.commands.executeCommand("browser.openUrl")
-      // oni.editors.activeEditor.activeBuffer.getLayerById("oni.browser")
-    }
+    click: () => oni.commands.executeCommand("browser.openUrl"),
   })
 
   const browserBackButton = new TouchBarButton({
     icon: icons.browserBack,
-    click: () => {
-      oni.commands.executeCommand("browser.goBack")
-    }
+    click: () => oni.commands.executeCommand("browser.goBack"),
   })
 
   const browserForwardButton = new TouchBarButton({
     icon: icons.browserForward,
-    click: () => {
-      oni.commands.executeCommand("browser.goForward")
-    }
+    click: () => oni.commands.executeCommand("browser.goForward"),
   })
 
   const browserDeveloperToolsButton = new TouchBarButton({
     icon: icons.debug,
-    click: () => {
-      oni.commands.executeCommand("browser.debug")
-    }
+    click: () => oni.commands.executeCommand("browser.debug"),
   })
 
-  const isBrowserBuffer = (buffer: Oni.Buffer) => {
-    let browserFilePath = /\/Browser\d+$/
-    return browserFilePath.test(buffer.filePath)
+  // language commands
+  const renameButton = new TouchBarButton({
+    icon: icons.rename,
+    click: () => oni.commands.executeCommand("editor.rename"),
+  })
+
+  const definitionButton = new TouchBarButton({
+    icon: icons.definition,
+    click: () => oni.commands.executeCommand("language.gotoDefinition"),
+  })
+
+  const formatButton = new TouchBarButton({
+    icon: icons.format,
+    click: () => oni.commands.executeCommand("autoformat.prettier"),
+  })
+
+  // debugging commands
+  const toggleRecording = () => {
+    if (oni.recorder.isRecording) {
+      oni.commands.executeCommand("keyDisplayer.hide")
+      oni.recorder.stopRecording()
+      recordButton.icon = icons.recordStart
+    } else {
+      oni.commands.executeCommand("keyDisplayer.show")
+      oni.recorder.startRecording()
+      recordButton.icon = icons.recordStop
+    }
+  }
+
+  const recordButton = new TouchBarButton({
+    icon: icons.recordStart,
+    backgroundColor: "#FF0000",
+    click: () => toggleRecording(),
+  })
+
+  const developerToolsButton = new TouchBarButton({
+    icon: icons.debug,
+    click: () => oni.commands.executeCommand("oni.debug.openDevTools"),
+  })
+
+  const reloadButton = new TouchBarButton({
+    icon: icons.reload,
+    click: () => oni.commands.executeCommand("oni.debug.reload"),
+  })
+
+  // filtering functions
+  const isBrowserBuffer = () => {
+    const browserFilePath = /\/Browser\d+$/
+    return browserFilePath.test(oni.editors.activeEditor.activeBuffer.filePath)
+  }
+
+  const languageCapabilities = () => {
+    const language = oni.editors.activeEditor.activeBuffer.language
+    return oni.language.getCapabilitiesForLanguage(language)
   }
 
   const buildTouchBar = async () => {
     let escapeItem = biggerEscButton
 
-    const sidebarActions = [sidebarButton, explorerButton, searchButton, browserButton]
+    const sidebarActions = [
+      sidebarButton,
+      explorerButton,
+      searchButton,
+      browserButton,
+    ]
     const languageActions = [renameButton, formatButton, definitionButton]
-    const browserActions = [browserBackButton, browserForwardButton, browserDeveloperToolsButton]
+    const browserActions = [
+      browserBackButton,
+      browserForwardButton,
+      browserDeveloperToolsButton,
+    ]
     const debugActions = [recordButton, developerToolsButton, reloadButton]
 
     let items = []
-    items = [...items,
+
+    items = [
+      ...items,
       ...sidebarActions,
       new TouchBarSpacer({ size: "flexible" }),
     ]
-    if (isBrowserBuffer(oni.editors.activeEditor.activeBuffer)) {
-      items = [ ...items, ...browserActions]
+
+    if (isBrowserBuffer()) {
+      items = [...items, ...browserActions]
     } else {
-      items = [ ...items, ...languageActions]
+      items = [...items, ...languageActions]
     }
 
-    items = [...items,
+    items = [
+      ...items,
       new TouchBarSpacer({ size: "flexible" }),
       ...debugActions,
     ]
